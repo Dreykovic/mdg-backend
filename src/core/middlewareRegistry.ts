f37ec100e771c3cf29096084eb3973d7b0dba286
dreykovic <dreybirewa@gmail.com>
2025-05-26 19:46:35 +0000

    [refactor-architecture-to-mvc-like] refactor: Remove unused validation middlewares to streamline middleware registry

--- src/core/middlewareRegistry.ts
+++ src/core/middlewareRegistry.ts
@@ -26,44 +26,6 @@ export class MiddlewareRegistry {
     // Cache middlewares
     this.middlewares.set('cache:300', this.createCacheMiddleware(300));
     this.middlewares.set('cache:600', this.createCacheMiddleware(600));
-
-    // Validation middlewares
-    this.middlewares.set(
-      'validateOwnership',
-      (req: any, res: any, next: any) => {
-        log('Validating ownership...');
-        next();
-      }
-    );
-    this.middlewares.set(
-      'validatePurchase',
-      (req: any, res: any, next: any) => {
-        log('Validating purchase...');
-        next();
-      }
-    );
-    this.middlewares.set(
-      'validateCategory',
-      (req: any, res: any, next: any) => {
-        log('Validating category...');
-        next();
-      }
-    );
-    this.middlewares.set('sanitizeQuery', (req: any, res: any, next: any) => {
-      log('Sanitizing query...');
-      next();
-    });
-    this.middlewares.set('confirmDeletion', (req: any, res: any, next: any) => {
-      log('Confirming deletion...');
-      next();
-    });
-    this.middlewares.set(
-      'validateBulkData',
-      (req: any, res: any, next: any) => {
-        log('Validating bulk data...');
-        next();
-      }
-    );
   }
 
   public get(name: string): any {
